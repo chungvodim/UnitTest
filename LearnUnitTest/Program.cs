b1996951a27793875d929fa5b6b704982af785f9
Long Nguyen <ba198647@gmail.com>
2016-08-01 08:57:31 +0800

    add

--- LearnUnitTest/Program.cs
+++ LearnUnitTest/Program.cs
@@ -10,6 +10,27 @@ namespace LearnUnitTest
     {
         static void Main(string[] args)
         {
+            string hello = "hello";
+            UpdateString(hello);
+            Console.WriteLine(hello);
+            MyClass myClass = new MyClass() { MyProperty = "fuck" };
+            UpdateString(myClass);
+            Console.WriteLine(myClass.MyProperty);
         }
+
+        private static void UpdateString(MyClass myClass)
+        {
+            myClass.MyProperty += " you";
+        }
+
+        private static void UpdateString(string hello)
+        {
+            hello = hello + " you";
+        }
+    }
+
+    public class MyClass
+    {
+        public string MyProperty { get; set; }
     }
 }
